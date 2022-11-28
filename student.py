65655df03bc595c964d2ae795b62c49456013fac
MagicDash91 <75797622+MagicDash91@users.noreply.github.com>
2022-11-28 12:39:13 +0700

    Update student.py

--- student.py
+++ student.py
@@ -13,7 +13,7 @@ st.write('Students Performance Dataset')
 grid_table = AgGrid(df1)
 st.dataframe(grid_table)
 
-series = df1['race/ethnicity'].value_counts()
+series = grid_table['race/ethnicity'].value_counts()
 df_race = pd.DataFrame(series)
 df_race = df_race.reset_index()  
 df_race.columns = ['race/ethnicity', 'Total']
@@ -25,18 +25,18 @@ pie_chart = px.pie(df_race,
 
 st.plotly_chart(pie_chart)
 
-dfavg = df1.groupby('race/ethnicity', as_index=False)['average score'].mean()
+dfavg = grid_table.groupby('race/ethnicity', as_index=False)['average score'].mean()
 bar_chart = px.bar(dfavg, x="race/ethnicity", y="average score", title="Average Score each group")
 st.plotly_chart(bar_chart)
 
-dfavgmath = df1.groupby('race/ethnicity', as_index=False)['math score'].mean()
+dfavgmath = grid_table.groupby('race/ethnicity', as_index=False)['math score'].mean()
 matbar_chart = px.bar(dfavgmath, x="race/ethnicity", y="math score", title="Average Math Score each group")
 st.plotly_chart(matbar_chart)
 
-dfavgreading = df1.groupby('race/ethnicity', as_index=False)['reading score'].mean()
+dfavgreading = grid_table.groupby('race/ethnicity', as_index=False)['reading score'].mean()
 readingbar_chart = px.bar(dfavgreading, x="race/ethnicity", y="reading score", title="Average Reading Score each group")
 st.plotly_chart(readingbar_chart)
 
-dfavgwriting = df1.groupby('race/ethnicity', as_index=False)['writing score'].mean()
+dfavgwriting = grid_table.groupby('race/ethnicity', as_index=False)['writing score'].mean()
 writingbar_chart = px.bar(dfavgwriting, x="race/ethnicity", y="writing score", title="Average Writing Score each group")
 st.plotly_chart(writingbar_chart)
